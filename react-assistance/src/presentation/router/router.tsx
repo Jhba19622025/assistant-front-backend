8dddf5b70845de8c6519c75b302954e6f6444ae7
jhbafaws <jhbafaws@gmail.com>
2025-09-13 20:53:54 -0300

    Feat: add frontend

---
+++ react-assistance/src/presentation/router/router.tsx
@@ -0,0 +1,33 @@
+import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { AssistantPage } from '../pages';
+import { DashboardLayout } from '../layouts/DashboardLayout';
+
+export const menuRoutes = [
+  
+  {
+    to: "/assistant",
+    icon: "fa-solid fa-user",
+    title: "Asistente",
+    description: "Información del asistente",
+    component: <AssistantPage />
+  },
+];
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      ...menuRoutes.map( route => ({
+        path: route.to,
+        element: route.component
+      })),
+      {
+        path: '',
+        element: <Navigate to={ menuRoutes[0].to } />
+      }
+
+    ],
+  }
+])
